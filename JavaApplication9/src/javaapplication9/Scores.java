21bdd1c94aec138b6600a711bfeec8b075d89cbf
Kevin Bell <bellKevin@pm.me>
2022-09-22 09:19:32 -0600

    Update Scores.java

--- JavaApplication9/src/javaapplication9/Scores.java
+++ JavaApplication9/src/javaapplication9/Scores.java
@@ -40,7 +40,6 @@ public class Scores implements Serializable {
         for (int i = 0; i < students.size(); i++) {
             System.out.println(students.get(i) + " ");
         } // end for loop
-        System.out.println();
     } // end display method
 //******************************************************************************
 
